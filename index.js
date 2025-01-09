5850186ef6623998e094038b0fdfac8a8b3b604a
baxodirabdugafforov7 <abdugafforovbaxodir2005@gmail.com>
2025-01-08 17:21:40 -0800

    added index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const dadName = "Dilmurod Mo'minov";
+const momName = "Dilbar Ortiqova";
+const broName = "Jaxongir Abdugapporov";
+const sisterName = "Oyshabonu Abdugapporova";
+const myName = "Baxodir Abdugafforov";
+
+let dadAge = 45;
+let momAge = 42;
+let broAge = 15;
+let sisterAge = 8;
+let myAge = 19;
+
+console.log("This is our Family:")
+
+console.log("My Dad's name is " + dadName + " and he is " + dadAge + " years old.");
+console.log("My Mom's name is " + momName + " and she is " + momAge + " years old.");
+console.log("My Bro's name is " + broName + " and he is " + broAge + " years old.");
+console.log("My Sister's name is " + sisterName + " and she is " + sisterAge + " years old.");
+console.log("My name is " + myName + " and I am " + myAge + " years old.");
